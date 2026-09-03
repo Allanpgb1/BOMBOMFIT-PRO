55b4d2726aefe7c84f81cdd4bd6e17f638c5dddc
Allan <allanpgb1@icloud.com>
2026-09-03 09:49:19 -0300

    Atualizar o main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'app/app.dart';
 import 'core/config/app_config.dart';
 import 'services/supabase_service.dart';
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SupabaseService.initialize(
     url: AppConfig.supabaseUrl,
-    anonKey: AppConfig.supabaseAnonKey,
+    publishableKey: AppConfig.supabaseAnonKey,
   );
-  runApp(const ProviderScope(child: BombomFitApp()));
+  runApp(
+    const ProviderScope(
+      child: BombomFitApp(),
+    ),
+  );
 }
